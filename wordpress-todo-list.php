b048f12fd893118200fc8be4f676f7f621059c48
Krzysztof Kubicius <kskubicius@gmail.com>
2021-05-23 12:48:56 +0200

    Create wordpress-todo-list.php

---
+++ wordpress-todo-list.php
@@ -0,0 +1,8 @@
+<?php
+/**
+* Plugin Name: WordPress To-do list
+* Plugin URI: https://github.com/kubicius/wordpress-todo-list
+* Description: WordPress To-do list.
+* Version: 0
+* Author: Krzysztof Kubicius
+* Author URI: https://kubicius.eu
