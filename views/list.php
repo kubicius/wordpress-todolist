c3185634d887334b4aafce343fe7981550bf43b3
Krzysztof Kubicius <kskubicius@gmail.com>
2021-05-30 15:36:29 +0200

    Updated styles

--- views/list.php
+++ views/list.php
@@ -9,9 +9,6 @@
             <div class="todolist__title--edit"  style="display: none;">
                 <input class="todolist__title-input" type="text" value="<?php echo $todolist['name'] ?>">
             </div>
-            <div class="todolist__task todolist__task--add">
-                <input type="text" placeholder="Add task" class="todolist__task-input--add">
-            </div>
             <?php foreach ($todolist['tasks'] as $task): ?>
             <div class="todolist__task" id="todolist_task_<?php echo $task['ID'] ?>">
                 <input type="checkbox" class="todolist__task-checkbox" <? echo $task['finished'] == 1 ? 'checked' : '' ?>>
@@ -21,6 +18,9 @@
                 <span class="todolist__task-button--delete dashicons dashicons-trash"></span>
             </div>
             <?php endforeach; ?>
+            <div class="todolist__task todolist__task--add">
+                <input type="text" placeholder="Add task" class="todolist__task-input--add">
+            </div>
         </div>
         <?php endforeach; ?>
         <div class="todolist__list todolist__list--new">
@@ -34,15 +34,15 @@
                 <input class="todolist__title-input--new" type="text" placeholder="Title">
                 <input class="todolist__title-input" type="text" placeholder="Title" style="display: none;">
             </div>
-            <div class="todolist__task todolist__task--add" style="display: none;">
-                <input type="text" placeholder="Add task" class="todolist__task-input--add">
-            </div>
             <div class="todolist__task todolist__task--pattern" style="display:none;">
                 <input type="checkbox" class="todolist__task-checkbox">
                 <span class="todolist__task-title"></span>
                 <input type="text" class="todolist__task-input--edit" style="display:none;">
                 <span class="todolist__task-button--edit dashicons dashicons-edit-large"></span>
                 <span class="todolist__task-button--delete dashicons dashicons-trash"></span>
+            </div>
+            <div class="todolist__task todolist__task--add" style="display: none;">
+                <input type="text" placeholder="Add task" class="todolist__task-input--add">
             </div>
         </div>
     </div>
