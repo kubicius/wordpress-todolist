ebc74160fd9034f58234172060da403f19863bc8
Krzysztof Kubicius <kskubicius@gmail.com>
2021-05-30 15:36:50 +0200

    Added url constant

--- Classes/Controllers/TodolistController.php
+++ Classes/Controllers/TodolistController.php
@@ -41,10 +41,10 @@ class TodolistController extends Controller{
     }
 
     public function show(){
-        wp_enqueue_style( 'styles', '/wp-content/plugins/todolist/resources/css/styles.css', false, 1, 'all');
-        wp_enqueue_script( 'Todolist', '/wp-content/plugins/todolist/resources/js/ajax/Todolist.js', false, 1, true);
-        wp_enqueue_script( 'TodolistTask', '/wp-content/plugins/todolist/resources/js/ajax/TodolistTask.js', false, 1, true);
-        wp_enqueue_script( 'listeners', '/wp-content/plugins/todolist/resources/js/listeners.js', false, 1, true);
+        wp_enqueue_style( 'styles', TODOLIST_URL . 'resources/css/styles.css', false, 1, 'all');
+        wp_enqueue_script( 'Todolist', TODOLIST_URL . 'resources/js/ajax/Todolist.js', false, 1, true);
+        wp_enqueue_script( 'TodolistTask', TODOLIST_URL . 'resources/js/ajax/TodolistTask.js', false, 1, true);
+        wp_enqueue_script( 'listeners', TODOLIST_URL . 'resources/js/listeners.js', false, 1, true);
 
         $todolistObj = new Todolist();
         $todolistTaskObj = new TodolistTask();
--- todolist.php
+++ todolist.php
@@ -13,6 +13,7 @@ defined( 'ABSPATH' ) || exit;
 
 // Defining constants.
 define('TODOLIST_PATH', plugin_dir_path( __FILE__ ));
+define('TODOLIST_URL', plugin_dir_url(__FILE__));
 define('TODOLIST_TITLE', 'To Do List');
 
 // Loading models.
